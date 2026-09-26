83a30395d9520126a3acbea5192de2f9ec47b811
bitbyrubayat <mdrubayatalam@gmail.com>
2026-09-26 17:56:10 +0600

    demo Issues Card designed according to figma

--- logic.js
+++ logic.js
@@ -12,3 +12,28 @@ tabButton.forEach((button)=>{
         button.classList.add("active");
     });
 });
+
+
+
+// ----------------login-----------------------
+
+const getUsername = document.getElementById("username");
+const getPassword = document.getElementById("password");
+
+const loginBtn = document.getElementById("login-btn");
+
+loginBtn.addEventListener("click", (e)=>{
+    
+    e.preventDefault();
+    if(getUsername.value === "admin" && getPassword.value === "admin123")
+    {
+        alert("logged In in Progress !!!");
+        window.location.href = "main_page.html"
+        
+
+    }
+    else
+       {
+        alert("invalid");
+       } 
+})
